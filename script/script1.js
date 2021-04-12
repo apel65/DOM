75358b67f64fe7f13818d947938988c9ae017f33
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-04-12 16:16:10 +0600

    added new these are textContent, lastChildd, querySelector, ntd, nth-child, setAttribute etc

--- script/script1.js
+++ script/script1.js
@@ -1,3 +1,21 @@
 document.title = `Play with dom ${new Date().toLocaleTimeString()}`;
 
-console.log(document.all[6]);
+const grandParent = document.querySelector(".container");
+// const parents = document.querySelector("#list");
+// const children = parents.children;
+// console.log(grandParent.children);
+
+const children = grandParent.querySelector(".item");
+
+const parent = children.parentElement;
+
+const elementDiv = document.createElement("div");
+
+elementDiv.textContent = "Hello new div";
+grandParent.appendChild(elementDiv);
+elementDiv.className = "yellow";
+
+document.querySelector(".yellow").style.backgroundColor = "green";
+elementDiv.setAttribute("id", "yellow");
+
+console.log(elementDiv);
